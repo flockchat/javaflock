3be7250c25fa66cf10abc6f049297dbc31ca89ef
prateek <prateeksultania@gmail.com>
2017-08-03 16:38:34 +0530

    Fix handling of collection type parameters

--- src/main/java/com/flock/api/ApiUtils.java
+++ src/main/java/com/flock/api/ApiUtils.java
@@ -1,13 +1,19 @@
 package com.flock.api;
 
+import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+import com.google.gson.Gson;
 
 class ApiUtils {
     static final String FLOCK_BASE_URL = "https://api.flock.co/v1";
+    private static final Gson gson = new Gson();
 
     static void putParam(Map<String, Object> map, String key, Object value) {
         if (key != null && value != null) {
+            if (value instanceof Collection) {
+                value = gson.toJson(value);
+            }
             map.put(key, value);
         }
     }
